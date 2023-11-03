5f98caeaa50851b86492c049f67a2a35c5d7482a
Вася <@DESKTOP-SFOGOU7>
2023-11-04 02:18:03 +0300

    Добавьте файлы проекта.

---
+++ laba8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string[] lines = File.ReadAllLines("Inlet.in");
+        string searchChar = lines[0].Split('_')[1];
+        char charToSearch = searchChar[0];
+        string lastLine = lines[lines.Length - 1];
+        string[] words = lastLine.Split(' ');
+        string secondLastWord = words[words.Length - 2];
+        int count = 0;
+        foreach (char c in secondLastWord)
+        {
+            if (char.ToLower(c) == char.ToLower(charToSearch))
+            {
+                count++;
+            }
+        }
+        File.WriteAllText("Outlet.out", count.ToString());
+    }
+}
